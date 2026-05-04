1c1b4315ed255a2c9560c4dce8dc6907f28ce4c3
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-05-04 13:18:55 -0300

    feat(main): add s3 bucket resource

--- day-01-iam/main.tf
+++ day-01-iam/main.tf
@@ -1,3 +1,16 @@
 # Day 01 — IAM
 # Spec: criar user, group, policy customizada com least privilege para acesso S3
 # Implemente aqui.
+# Docs: https://claude.ai/chat/b56e5225-534f-45f9-b609-876f629b663c
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "${var.s3_bucket_name}-${var.aws.region}-${random_id.suffix.hex}"
+
+  tags = var.s3_tags
+}
+
+resource "resource random_id name {
+  byte_length = 4
+}
+"
+
--- day-01-iam/variables.tf
+++ day-01-iam/variables.tf
@@ -1,4 +1,21 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
+variable aws_region {
+  type        = string
+  default     = "us-east-2"
+  description = "Region of AWS"
 }
+
+
+variable s3_bucket_name {
+  type        = string
+  default     = "s3_bucket"
+  description = "S3 bucket name"
+}
+
+variable s3_tags {
+  type        = map(string)
+  default     = {
+    IAC = true
+  }
+  description = "description"
+}
+
